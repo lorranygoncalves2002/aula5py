f10e3c02e8c7c7defbecd8cec72a4498a1c19228
lorranygoncalves2002 <146742359+lorranygoncalves2002@users.noreply.github.com>
2023-10-04 11:11:21 -0300

    Add files via upload

---
+++ funcao1.py
@@ -0,0 +1,20 @@
+def minha_funcao ( ):
+    print('chamei a função')
+
+    minha_funcao()
+
+    #---------------
+
+def minha_funcao2(nome):
+
+    print(nome + 'siqueira santos')
+    minha_funcao2('lola')
+    minha_funcao2('ana')
+    minha_funcao2('dennis')
+
+saldo = 1000
+def sacar(valor):
+    saldo = 1000 - valor
+    print(saldo)
+
+
